9eeff83a3e9ef925aef38317e0ce695038a8dee8
christphe <christphe@ffxmail.com>
2024-09-26 10:55:42 +0200

    use graphicsMagick

--- scripts/preview.js
+++ scripts/preview.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const getDirectories = () => {
     const srcpath = path.join(__dirname, '../pdf');
-    return fs.readdirSync(srcpath);
+  return fs.readdirSync(srcpath);
 };
 
 const getTemplateName = PDF => {
@@ -13,21 +13,23 @@ const getTemplateName = PDF => {
 };
 
 const convert = async (PDF) => {
-    const pdfImage = new PDFImage(path.join(__dirname, '../pdf/' + PDF));
-    await pdfImage.convertPage(0);
+    const pdfImage = new PDFImage(path.join(__dirname, '../pdf/' + PDF, {
+      graphicsMagick: true
+    }));
+  await pdfImage.convertPage(0);
 };
 
 const directories = getDirectories();
 directories.forEach(async (dir) => {
-    try {
-        await convert(dir);
-    } catch (e) {
-        console.dir(e);
-    }
+  try {
+    await convert(dir);
+  } catch (e) {
+    console.dir(e);
+  }
     const source = path.join(__dirname, '../pdf/' + getTemplateName(dir) + '-0.png');
     const output = path.join(__dirname, '../src/assets/preview/resume-' + getTemplateName(dir) + '.png');
-    console.log(output);
+  console.log(output);
     mv(source, output, function (err) {
-        if (err) console.dir(err);
-    });
+    if (err) console.dir(err);
+  });
 });
